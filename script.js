4682658f24421acdf084b2b612ade080790ada5e
Veronica Harris <veronica.n.harris92@gmail.com>
2021-03-18 20:58:35 -0400

    changes to javascript i wish the smooth brain well

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// var options = []
 
 var length = document.querySelector("#length");
 var lowerCase = document.querySelector("#lowercase");
@@ -8,50 +9,85 @@ var numberS = document.querySelector("#numbers");
 var result = document.querySelector("#result");
 var generateBtn = document.getElementById("generateBtn");
 
-var randomFunc = {
-  lower: lowerCase,
-  upper: upperCase,
-  number: numberS,
-  symbols: randomSymbols,
+var length = length.value
+var lowerCase = lowerCase.value
+var upperCase = upperCase.value
+var randomSymbols = randomSymbols.value
+var numberS = numberS.value
+
+// var password = generatePassword(length, lowerCase, upperCase, randomSymbols, numberS)
+
+// var randomFunc = {
+//   lower: lowerCase,
+//   upper: upperCase,
+//   number: numberS,
+//   symbols: randomSymbols,
+// }
+
+var userChoice = window.alert("Halt! Would you like a password?");
+
+var message;
+if (!userChoice) {
+  message = "Goodbye!";
 }
 
-window.alert(
-  "Would you like a password?"
-);
+
+function generatePassword(length, lowerCase, upperCase, randomSymbols, numberS) {
+}
+
+var userChoice = window.confirm("Would you like lowercase letters?") {
+  function lowerCase () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+  }
+}; if else {
+  var userChoice;
+ if (window.confirm("Would you like uppercase letters?")) {
+  function upperCase () {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+  }
+}; if else {
+  var userChoice = window.confirm("Would you like numbers?")
+}; if else {
+  var userChoice = window.confirm("Would you like symbols?")
+}
+
+// }
+
+// var userChoice = window.confirm("Would you like lowercase letters?");
+
+// var userChoice = window.confirm("Would you like numbers?");
+
+// var userChoice = window.confirm("Would you like symbols?");
+
+
 
 // Generate Events
 // I want to click yes or no when asked a question
 // generateBtn.addEventListener("click",
 //   length: 
 
-  passwords.innerText = generatePassword(
-    length,
-    lowerCase,
-    upperCase,
-    numberS,
-    randomSymbols,
-  );
-// });
+//   passwords.innerText = generatePassword(
+//     length,
+//     lowerCase,
+//     upperCase,
+//     numberS,
+//     randomSymbols,
+//   );
+// // });
  
-function generatePassword(lower, upper, number, symbol, length) {
-  let result = "",
+// function generatePassword(lower, upper, number, symbol, length) {
+//   let result = "",
 
-   const typesCount = lower + upper + number + symbol;
+//    const typesCount = lower + upper + number + symbol;
 
-   const typesArr = [lower, upper, number, and symbol];
+//    const typesArr = [lower, upper, number, and symbol];
 
+
+// }
+
+function generatePassword(length, lowerCase, upperCase, randomSymbols, numberS) {
 
 }
-
-var userChoice = window.confirm("Would you like uppercase letters?");
-
-
-var userChoice = window.confirm("Would you like lowercase letters?");
-
-var userChoice = window.confirm("Would you like numbers?");
-
-var userChoice = window.confirm("Would you like symbols?");
-
 
 function lowerCase () {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -64,5 +100,5 @@ function numberS () {
 }
 function randomSymbols () {
   var symbols = '!@#$%^&*()+=<>?/'
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+    return symbols[Math.floor(Math.random() * symbols.length)] 
+  }
